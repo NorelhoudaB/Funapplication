19cb112ff43af8e866c1f4c5de348d3f87ba30de
norel_2b285at <norel_2b285at@NOR11>
2024-12-04 15:37:19 +0100

    Add project files.

---
+++ Funapplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Funapplication.Models;
+
+namespace Funapplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Funapplication.Models.Riddle>? Riddle { get; set; }
+    }
+}
---
+++ Funapplication/Models/Riddle.cs
@@ -0,0 +1,16 @@
+﻿namespace Funapplication.Models
+{
+    public class Riddle
+    {
+        public int Id { get; set; }
+        public string RiddleQuestion{ get; set; }
+        public string RiddleAnswer{ get; set; }
+        public string Category { get; set; }
+        public Riddle()
+        {
+
+        }
+
+
+    }
+}
